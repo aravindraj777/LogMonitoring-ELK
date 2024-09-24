9b451a06ad9e85f078df0dd5fa38f84f35df7070
Aravind <aravindmunna240@gmail.com>
2024-09-25 00:37:53 +0530

    Making a user class and making three folders for elk configurations

--- src/main/java/com/buildlive/logAggregation/controller/UserController.java
+++ src/main/java/com/buildlive/logAggregation/controller/UserController.java
@@ -1,2 +1,36 @@
-package com.buildlive.logAggregation.controller;public class UserController {
+package com.buildlive.logAggregation.controller;
+
+
+import com.buildlive.logAggregation.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@Slf4j
+public class UserController {
+
+    @GetMapping("users/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable String id){
+
+        Long userId;
+
+        try {
+            userId = Long.parseLong(id);
+        }
+        catch (NumberFormatException e){
+            String message = String.format("Invalid user id: %s. Please enter a valid numeric id",id);
+            log.error(message );
+            return ResponseEntity.badRequest().body(message);
+        }
+
+        log.info("Request with id = [{}]",userId);
+        final User user = new User(userId,"name"+userId, LocalDateTime.now());
+        log.info("Data retrieved id=[{}]",userId);
+        return ResponseEntity.ok(user);
+    }
 }
--- src/main/java/com/buildlive/logAggregation/domain/User.java
+++ src/main/java/com/buildlive/logAggregation/domain/User.java
@@ -1,2 +1,19 @@
-package com.buildlive.logAggregation.domain;public class User {
+package com.buildlive.logAggregation.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private LocalDateTime birthDetails;
 }
